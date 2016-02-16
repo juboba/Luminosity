9a9fbfab2e718f70b78a66291383f0bf89575c9e
Áureo Ares <aureo.ares@gmail.com>
2016-02-16 11:56:47 +0000

    First apiDoc implementation for TaskController.

---
+++ doc/api/api_project.js
@@ -0,0 +1,15 @@
+define({
+  "name": "Super API",
+  "version": "0.1.0",
+  "description": "Master of all APIs",
+  "title": "Manuel es guay",
+  "url": "http://localhost:80/api/v0_01",
+  "sampleUrl": false,
+  "apidoc": "0.2.0",
+  "generator": {
+    "name": "apidoc",
+    "time": "2016-02-16T11:54:48.984Z",
+    "url": "http://apidocjs.com",
+    "version": "0.15.0"
+  }
+});
