08a368a6d8ef20c1e88f6f3829d301a333d1a04e
Yhensel <yhensel@gmail.com>
2016-02-16 11:01:34 +0100

    Implementing all task for a user

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -22,6 +22,8 @@ $app->group(['prefix' => 'api/login', 'namespace' => 'App\Http\Controllers'], fu
 $app->group(['prefix' => 'api/v0_01/users', 'namespace' => 'App\Http\Controllers', 'middleware' => 'auth'], function ($app) {
     $app->get('/', 'UserController@index');
     $app->get('{id}', 'UserController@show');
+    $app->get('{id}/tasks', 'TaskController@allForUser');
+
     $app->put('{id}', 'UserController@update');
     $app->delete('{id}', 'UserController@destroy');
 
